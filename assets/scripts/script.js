3e9e2692f95ae430d3586b8e03cf4656ac6a76a4
westtle <103447524+westtle@users.noreply.github.com>
2024-02-07 02:08:51 +0700

    added download note functionality

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,6 +2,24 @@
 const inputTitle = document.querySelector(".simple-note__input--title");
 const inputBody = document.querySelector(".simple-note__input--body");
 
+const downloadButton = document.querySelector(".settings__button--download");
+
+function downloadNote() {
+    const noteTitle = inputTitle.value || "Simple Note";
+    const noteBody = inputBody.value;
+
+    const element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(noteBody));
+    element.setAttribute('download', noteTitle);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+};
+
 // Local Storage.
 const note_title = "Note_Title";
 const note_body = "Note_Body";
@@ -37,6 +55,7 @@ const debouncedSaveBody = debounce(saveBody);
 
 inputTitle.addEventListener("input", debouncedSaveTitle);
 inputBody.addEventListener("input", debouncedSaveBody);
+downloadButton.addEventListener("click", downloadNote);
 
 document.addEventListener("DOMContentLoaded", () => {
     loadNote();
@@ -64,19 +83,3 @@ document.addEventListener("DOMContentLoaded", () => {
 //     saveTitle();
 //     saveBody();
 // };
-
-// function downloadNote() {
-//     let title = noteTitle.value || "Simple Note";
-//     let text = noteBody.value;
-
-//     let element = document.createElement('a');
-//     element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-//     element.setAttribute('download', title);
-
-//     element.style.display = 'none';
-//     document.body.appendChild(element);
-
-//     element.click(); // Not my code, I just changed it a bit haha.
-
-//     document.body.removeChild(element);
-// }
