f7d2b21a0d1d56881dc3a70688d3bfbb24f0e77e
westtle <103447524+westtle@users.noreply.github.com>
2024-02-07 00:01:46 +0700

    used separate function for settingsToggle

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,19 +2,21 @@ const settingsToggle = document.querySelector(".settings__button--toggle-setting
 const settingsList = document.querySelector(".settings__buttons");
 const settingsItems = settingsList.querySelectorAll("a, button");
 
-settingsToggle.addEventListener("click", () => {
+function toggleSettings() {
     const isExpanded = (settingsToggle.getAttribute("aria-expanded") == "true");
 
     if (isExpanded) {
         settingsToggle.setAttribute("aria-expanded", false);
         settingsList.setAttribute("aria-hidden", true);
-        settingsItems.forEach(item => {item.tabIndex = -1;});
+        settingsItems.forEach(item => {item.tabIndex = -1});
     } else {
         settingsToggle.setAttribute("aria-expanded", true);
         settingsList.setAttribute("aria-hidden", false);
-        settingsItems.forEach(item => {item.tabIndex = 0;});
+        settingsItems.forEach(item => {item.tabIndex = 0});
     };
-});
+};
+
+settingsToggle.addEventListener("click", toggleSettings);
 
 // let timerId;
 
